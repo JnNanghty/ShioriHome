96203422a8b0b9565dd59882686f0bafb486ae86
wjn <961331258@qq.com>
2026-01-22 16:51:16 +0800

    新增   随机播放按钮，每隔500毫秒就随机播放一段音频

--- src/views/VoiceView.vue
+++ src/views/VoiceView.vue
@@ -1,16 +1,49 @@
 <script setup>
 import {voice} from "@/assets/voice.js";
+import {computed, ref} from "vue";
 
 const modules = import.meta.glob('@/assets/**/*.mp3', {eager: true, import: 'default'})
 const playAudio = (url) => {
   const audio = new Audio(modules[`/src/assets/voice/${url}`])
   audio.play()
 }
+
+const isPlaying = ref(false)
+const randomText = computed(() => isPlaying.value ? '我的电脑里住不下这么多小栞' : '点一下就会出现好多小栞')
+const randomPlay = async () => {
+  isPlaying.value = !isPlaying.value;
+  if (isPlaying.value) {
+    let keys = Object.keys(modules)
+    while (true) {
+      if (!isPlaying.value) {
+        break
+      }
+      const index = Math.floor(Math.random() * keys.length)
+      let audioUrl = modules[keys[index]]
+      const audio = new Audio(audioUrl);
+      audio.play()
+      await delay(500)
+    }
+  }
+}
+
+const delay = async (time) => {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
 </script>
 
 <template>
   <div>
     <div class="content-title">小栞按钮</div>
+    <div class="random-bar">
+      <el-button
+          plain
+          size="large"
+          :type="isPlaying ? 'danger' : 'primary'"
+          @click="randomPlay">
+        {{ randomText }}
+      </el-button>
+    </div>
     <div class="voice-content">
       <div v-for="item in voice" class="voice-item">
         <el-button plain size="large" type="primary" @click="playAudio(item.url)">{{ item.name }}</el-button>
@@ -27,5 +60,10 @@ const playAudio = (url) => {
   gap: 40px;
   align-items: flex-start;
   flex-wrap: wrap;
+}
+
+.random-bar {
+  margin-top: 20px;
+  text-align: center;
 }
 </style>
